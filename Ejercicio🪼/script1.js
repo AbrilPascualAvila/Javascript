e66078061da2fbba77122099f9345ad2bd9d1449
Abril Pascual Avila <aprilpascual1125@gmail.com>
2025-09-26 12:56:13 +0200

    primera part

---
+++ Ejercicio🪼/script1.js
@@ -0,0 +1,11 @@
+const array = ['🍔','🌯','🍣','🍕','🍜','🍱','🍙','🍘','🥩'];
+
+const indexPizza = array.indexOf('🍕');
+for i in array;
+    array[i] = '🍺';
+
+    
+if 🍕 = 🍕;
+    replace (🍺)
+
+console.log(array);
